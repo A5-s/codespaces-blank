71d30989cacb11965085eda8b03c4b0505447f6d
A5-s <palmdog.buisness@gmail.com>
2025-09-13 21:03:15 -0500

    Update auth.js

--- ad-manager/routes/auth.js
+++ ad-manager/routes/auth.js
@@ -173,7 +173,7 @@ router.post("/login-web", async (req, res) => {
     const token = signSession(user);
     res.cookie("session", token, devCookie);
 
-    return res.redirect(user.role === "admin" ? "/admin" : "/business");
+    return res.redirect(user.role === "admin" ? "/admin" : "system" ? "/system" : "/business");
   } catch (e) {
     console.error(e);
     return res.redirect("/login.html?err=server");
